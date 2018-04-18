ca3734429ea8ae890e1e212588f1b5a9bdd90380
Elyes Turki <elyestu@gmail.com>
2018-04-18 12:12:31 +0100

    Utilisation de HttpClient - angular5

--- src/app/services/posts-list.service.ts
+++ src/app/services/posts-list.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+
+// import { Http, Response } from '@angular/http';
 import { IPost } from '../domain/ipost';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -7,13 +9,13 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class PostsListService {
 
-  URL = "https://jsonplaceholder.typicode.com/posts";
-  constructor( private _http: Http) { }
+  URL = 'https://jsonplaceholder.typicode.com/posts';
+  constructor( private _http: HttpClient) { }
 
   getAllPosts(): Observable<IPost[] > {
     return this._http.
-    get(this.URL).
-    map((resp: Response) => <IPost[]> resp.json());
+    get<IPost[]>(this.URL);
+   // map((resp: Response) => <IPost[]> resp.json());
 
   /*  return [
       {
